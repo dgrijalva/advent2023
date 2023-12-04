0277aaf7e7974f996b7cb5df996e3daa825a3a7e
Dave Grijalva <grijalva@gmail.com>
2023-12-04 13:52:09 -0800

    day 4 part 2

--- src/puzzle/day_04.rs
+++ src/puzzle/day_04.rs
@@ -1,5 +1,5 @@
 use super::Puzzle;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day04;
 
@@ -17,21 +17,52 @@ impl Puzzle for Day04 {
         let cards = Self::parse(input);
         let score = cards
             .into_iter()
-            .map(|card| {
-                card.values
-                    .iter()
-                    .filter(|v| card.winning.contains(v))
-                    .count()
-            })
+            .map(|card| card.winning_count())
             .filter(|&c| c > 0)
             .map(|c| 2usize.pow(c as u32 - 1))
             .sum::<usize>();
         Ok(score.to_string())
     }
 
-    fn part_two(&self, _input: &str) -> super::PuzzleResult {
-        todo!("implement part two")
+    fn part_two(&self, input: &str) -> super::PuzzleResult {
+        let cards = Self::parse(input);
+        let scores: Vec<(usize, usize)> = cards
+            .iter()
+            .map(|card| card.winning_count())
+            .enumerate()
+            .collect();
+        let mut card_count = HashMap::new();
+        for (idx, score) in &scores {
+            // insert original card
+            let count = inc(&mut card_count, *idx, 1);
+            // insert won cards
+            for i in (*idx + 1)..scores.len().min(*score + idx + 1) {
+                inc(&mut card_count, i, count);
+            }
+        }
+
+        let result = card_count
+            .iter()
+            .filter(|(idx, _)| **idx < cards.len())
+            .map(|(_, v)| v)
+            .sum::<usize>();
+        Ok(result.to_string())
     }
+}
+
+impl Card {
+    fn winning_count(&self) -> usize {
+        self.values
+            .iter()
+            .filter(|v| self.winning.contains(v))
+            .count()
+    }
+}
+
+fn inc(data: &mut HashMap<usize, usize>, idx: usize, c: usize) -> usize {
+    let count = data.entry(idx).or_insert(0);
+    *count += c;
+    *count
 }
 
 impl Day04 {

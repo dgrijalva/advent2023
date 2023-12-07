3c646921aedb3e7474b56001e0555910d0ecb07b
Dave Grijalva <grijalva@gmail.com>
2023-12-07 13:35:33 -0800

    day 6 part 2 - too fast. abandoning binary search

--- src/puzzle/day_06.rs
+++ src/puzzle/day_06.rs
@@ -1,10 +1,10 @@
 use super::Puzzle;
-use std::cmp::Ordering;
 
 pub struct Day06;
 
 // Input is so small, not bothering with a parser
 const INPUT: &'static [(usize, usize)] = &[(46, 347), (82, 1522), (84, 1406), (79, 1471)];
+const INPUT_PART_2: (usize, usize) = (46828479, 347152214061471);
 
 impl Puzzle for Day06 {
     fn new(_ops: &super::RootOpt) -> Box<dyn Puzzle> {
@@ -26,54 +26,13 @@ impl Puzzle for Day06 {
     }
 
     fn part_two(&self, _input: &str) -> super::PuzzleResult {
-        todo!("implement part two")
+        let (time, best) = INPUT_PART_2;
+        let result = (1..=time)
+            .into_iter()
+            .filter(|x| distance(*x, time) > best)
+            .count();
+        Ok(result.to_string())
     }
-}
-
-/// Returns the begginning and end of the sequence which produces a winning score
-fn winning_solutions(time: usize, best: usize) -> (usize, usize) {
-    let mut start: usize;
-    let mut end: usize;
-    let mut mid = time / 2;
-    // Binary search to find some point where the distance is greater than the best
-    loop {
-        match probe(time, best, mid) {
-            Ordering::Equal => {
-                break;
-            }
-            Ordering::Less => {
-                mid = mid + (time - mid) / 2;
-            }
-            Ordering::Greater => {
-                mid = mid / 2;
-            }
-        }
-    }
-
-    todo!()
-}
-
-fn search(best: usize, start: usize, end: usize) -> (usize, usize) {
-    todo!()
-}
-
-/// Check to see if the specified distance is a winning one
-/// If not, looks ahead and back to see which direction to check next
-fn probe(time: usize, best: usize, check: usize) -> Ordering {
-    let dist = distance(check, time);
-    if dist > best {
-        return Ordering::Equal;
-    }
-
-    if check > 1 {
-        if dist < distance(check - 1, time) {
-            return Ordering::Less;
-        }
-    }
-    if check >= time {
-        panic!("distance is greater than time");
-    }
-    return Ordering::Greater;
 }
 
 /// For each whole millisecond you spend at the beginning of the race holding down

2f23ba4d1fa47d7eb2798ae9ee97323b14f1908c
Dave Grijalva <grijalva@gmail.com>
2023-12-22 16:58:41 -0800

    day 12 part 2 complete. memoization + zero copy

--- src/puzzle/day_12.rs
+++ src/puzzle/day_12.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use super::Puzzle;
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 pub struct Day12;
 
@@ -12,7 +12,7 @@ struct Sequence {
     runs: Vec<usize>,
 }
 
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy, Hash)]
 enum State {
     Damaged,
     Operational,
@@ -26,7 +26,11 @@ impl Puzzle for Day12 {
 
     fn part_one(&self, input: &str) -> super::PuzzleResult {
         let data = parse_input(input);
-        let result = data.into_iter().map(|s| s.arrangements()).sum::<usize>();
+        let result = data
+            .into_iter()
+            .map(|s| s.arrangements())
+            .inspect(|v| println!("{v}"))
+            .sum::<usize>();
         Ok(result.to_string())
     }
 
@@ -61,157 +65,61 @@ impl Sequence {
 
     fn arrangements(&self) -> usize {
         // println!("start {:?} {:?}", &self.states, &self.runs);
-        Self::possible_arrangements(&self.states, &self.runs)
+        let mut memo = Default::default();
+        Self::possible_arrangements(&mut memo, &self.states, &self.runs)
     }
 
-    fn possible_arrangements(states: &[State], runs: &[usize]) -> usize {
-        // find index of first unknown state
-        let Some(idx) = states
-            .iter()
-            .enumerate()
-            .find(|(_, &s)| s == State::Unknown)
-            .map(|(i, _)| i)
-        else {
-            if Self::valid_for(states, runs).is_some() {
-                // println!("Valid {:?} {:?}", states, runs);
+    fn possible_arrangements<'a, 'b>(
+        memo: &'b mut HashMap<(&'a [State], &'a [usize]), usize>,
+        states: &'a [State],
+        runs: &'a [usize],
+    ) -> usize {
+        if let Some(v) = memo.get(&(states, runs)) {
+            return *v;
+        }
+
+        // if there are no more runs, there can't be any more damaged nodes
+        if runs.is_empty() {
+            if states.contains(&State::Damaged) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        // if states is empty, there can't be any more unconsumed runs
+        if states.is_empty() {
+            if runs.is_empty() {
                 return 1;
             } else {
                 return 0;
             }
-        };
+        }
 
         // recursive depth first search
         let mut sum = 0usize;
-        let mut data = states.to_vec();
-        for s in [State::Damaged, State::Operational] {
-            data[idx] = s;
-            // short circuit if already invalid
-            if let Some((data_len, runs_len)) = Self::valid_for(&data, &runs) {
-                // println!("{} {}", data_len, runs_len);
-                // println!("Checking {:?} {:?}", &data[data_len..], &runs[runs_len..]);
-                sum += Self::possible_arrangements(&data[data_len..], &runs[runs_len..]);
+        // Try the next state as . by just stepping forward
+        if [State::Unknown, State::Operational].contains(&states[0]) {
+            sum += Self::possible_arrangements(memo, &states[1..], runs);
+        }
+        // Try the next state as # by trying to consume the expected run length
+        if [State::Unknown, State::Damaged].contains(&states[0]) {
+            let next = runs[0];
+            if states.len() >= next // There are enough states left
+                && !states[..next].contains(&State::Operational)
+            {
+                // the run isn't too long
+                if states.len() == next {
+                    sum += Self::possible_arrangements(memo, &states[(next)..], &runs[1..]);
+                } else if states[next] != State::Damaged {
+                    sum += Self::possible_arrangements(memo, &states[(next + 1)..], &runs[1..]);
+                }
             }
         }
+
+        memo.insert((states, runs), sum);
+        // println!("{states:?} {runs:?} {sum}");
         sum
-    }
-
-    /// Checks that the sequence is valid and returns the number of complete
-    /// entities that can be consumed for each input (so we don't have to keep checking them)
-    fn valid_for(states: &[State], runs: &[usize]) -> Option<(usize, usize)> {
-        let mut run_iter = runs.iter();
-        let mut in_run = None;
-        let mut run_len = 0usize;
-        let mut consumed_runs = 0usize;
-        let mut consumed_states = 0usize;
-        for (idx, state) in states.iter().copied().enumerate() {
-            match state {
-                State::Unknown => return Some((consumed_states, consumed_runs)),
-                State::Operational => {
-                    // end run, if there is one
-                    if let Some(&run) = in_run {
-                        // make sure run was expected len
-                        if run_len != run {
-                            return None;
-                        }
-                        consumed_runs += 1;
-                    }
-                    in_run = None;
-                    run_len = 0;
-                    consumed_states = idx;
-                }
-                State::Damaged => {
-                    if in_run.is_none() {
-                        in_run = run_iter.next();
-                        // no more runs
-                        if in_run.is_none() {
-                            return None;
-                        }
-                    }
-                    run_len += 1;
-                }
-            }
-        }
-        // catch the last run if it goes to the end
-        if let Some(&run) = in_run {
-            // make sure run was expected len
-            if run_len != run {
-                return None;
-            }
-            consumed_runs += 1;
-            consumed_states = states.len();
-        }
-        if run_iter.next().is_some() {
-            return None;
-        }
-        return Some((consumed_states, consumed_runs));
-    }
-
-    fn valid(&self, states: &[State]) -> bool {
-        let mut runs = self.runs.iter();
-        let mut in_run = None;
-        let mut run_len = 0usize;
-        for state in states {
-            match state {
-                State::Unknown => return true, // TODO
-                State::Operational => {
-                    // end run, if there is one
-                    if let Some(&run) = in_run {
-                        // make sure run was expected len
-                        if run_len != run {
-                            return false;
-                        }
-                    }
-                    in_run = None;
-                    run_len = 0;
-                }
-                State::Damaged => {
-                    if in_run.is_none() {
-                        in_run = runs.next();
-                        // no more runs
-                        if in_run.is_none() {
-                            return false;
-                        }
-                    }
-                    run_len += 1;
-                }
-            }
-        }
-        // catch the last run if it goes to the end
-        if let Some(&run) = in_run {
-            // make sure run was expected len
-            if run_len != run {
-                return false;
-            }
-        }
-        if runs.next().is_some() {
-            return false;
-        }
-        true
-    }
-
-    /// An alternate approach to validation, used to double-check my work but no longer needed
-    #[allow(unused)]
-    fn run_lengths(states: &[State]) -> Vec<usize> {
-        let mut lengths = vec![];
-        let mut run_len = 0usize;
-        for s in states {
-            match s {
-                State::Unknown => unreachable!("only use this on complete states"),
-                State::Damaged => {
-                    run_len += 1;
-                }
-                State::Operational => {
-                    if run_len > 0 {
-                        lengths.push(run_len);
-                    }
-                    run_len = 0;
-                }
-            }
-        }
-        if run_len > 0 {
-            lengths.push(run_len);
-        }
-        lengths
     }
 }
 

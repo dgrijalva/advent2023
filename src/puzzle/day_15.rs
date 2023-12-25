bb8077b93e0cb0b0538f1162cd2b3af4df5a6020
Dave Grijalva <grijalva@gmail.com>
2023-12-25 15:34:14 -0800

    day 15 part 2

--- src/puzzle/day_15.rs
+++ src/puzzle/day_15.rs
@@ -1,6 +1,13 @@
+use itertools::Itertools;
+
 use super::Puzzle;
 
 pub struct Day15;
+
+enum Instruction {
+    Insert(u8),
+    Remove,
+}
 
 impl Puzzle for Day15 {
     fn new(_ops: &super::RootOpt) -> Box<dyn Puzzle> {
@@ -19,8 +26,44 @@ impl Puzzle for Day15 {
         Ok(result.to_string())
     }
 
-    fn part_two(&self, _input: &str) -> super::PuzzleResult {
-        todo!("implement part two")
+    fn part_two(&self, input: &str) -> super::PuzzleResult {
+        let steps = parse_instructions(input);
+        let mut boxen: Vec<Vec<(String, usize)>> = Vec::from_iter((0..255).map(|_| vec![]));
+
+        // Process the instructions
+        for (id, box_idx, instruction) in steps {
+            match instruction {
+                Instruction::Remove => boxen[box_idx].retain(|v| v.0 != id),
+                Instruction::Insert(val) => {
+                    let found = boxen[box_idx]
+                        .iter()
+                        .enumerate()
+                        .find(|(_, (i, _))| i == &id)
+                        .map(|(idx, _)| idx);
+                    if let Some(found) = found {
+                        boxen[box_idx][found] = (id, val as usize);
+                    } else {
+                        boxen[box_idx].push((id, val as usize));
+                    }
+                }
+            }
+        }
+
+        // Compute the power
+        // For each box
+        let power = boxen
+            .into_iter()
+            .enumerate()
+            .map(|(box_idx, boxx)| {
+                // For each lens
+                boxx.into_iter()
+                    .enumerate()
+                    .map(|(lens_idx, (_, lens_power))| (box_idx + 1) * (lens_idx + 1) * lens_power)
+                    .sum::<usize>()
+            })
+            .sum::<usize>();
+
+        Ok(power.to_string())
     }
 }
 
@@ -32,4 +75,26 @@ fn hash(input: &str) -> usize {
         hash = hash % 256;
     }
     return hash;
+}
+
+fn parse_instructions(input: &str) -> Vec<(String, usize, Instruction)> {
+    input
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|i| {
+            if i.contains('-') {
+                let id = i.split('-').next().unwrap().to_string();
+                (id.clone(), hash(&id), Instruction::Remove)
+            } else {
+                let parts = i.split('=').collect_vec();
+                (
+                    parts[0].to_string(),
+                    hash(parts[0]),
+                    Instruction::Insert(parts[1].parse().unwrap()),
+                )
+            }
+        })
+        .collect_vec()
 }
